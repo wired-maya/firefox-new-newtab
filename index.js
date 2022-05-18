c90a10d8f7708a119254800f5c9add914043b838
Daniel Hodul <dhodul@gmail.com>
2022-05-17 23:05:04 -0700

    Initial commit 2

---
+++ index.js
@@ -0,0 +1,80 @@
+// Get required elements
+const searchQuery = document.getElementById('query');
+const selectorButton = document.getElementById('selector-button');
+const selectorButtonGoogle = document.getElementById('selector-button-google');
+const selectorButtonArchWiki = document.getElementById('selector-button-arch-wiki');
+const selectorButtonInternetArchive = document.getElementById('selector-button-internet-archive');
+const selectorButtonWaybackMachine = document.getElementById('selector-button-wayback-machine');
+const selectorButtonWikipedia = document.getElementById('selector-button-wikipedia');
+const selectorButtoWikimediaCommons = document.getElementById('selector-button-wikimedia-commons');
+
+// Base URLs used for searching
+const googleBaseUrl = 'https://www.google.com/search?q=';
+const archWikiBaseUrl = 'https://wiki.archlinux.org/index.php?search=';
+const internetArchiveBaseUrl = 'https://archive.org/search.php?query=';
+const waybackMachineBaseUrl = 'https://web.archive.org/web/*/';
+const wikipediaBaseUrl = 'https://en.wikipedia.org/w/index.php?search=';
+const wikimediaCommonsBaseUrl = 'https://commons.wikimedia.org/w/index.php?title=Special:MediaSearch&search=';
+
+let selectedBaseUrl;
+
+// Perform the actual search
+function performSearch(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    
+        const url = selectedBaseUrl + searchQuery.value;
+        const win = window.open(url, '_self');
+        win.focus;
+    }
+}
+
+// Change search engine dropdown to reflect currently selected engine
+function changeSelectedSearchEngine(event) {
+    event.preventDefault();
+
+    switch(event.target.id) {
+        case "selector-button-google":
+            selectorButton.style.backgroundImage = "url('imgs/logos/google\ logo.png')";
+            selectedBaseUrl = googleBaseUrl;
+            searchQuery.placeholder = "Search with Google";
+            break;
+        case "selector-button-arch-wiki":
+            selectorButton.style.backgroundImage = "url('imgs/logos/arch\ logo.png')";
+            selectedBaseUrl = archWikiBaseUrl;
+            searchQuery.placeholder = "Search the Arch Wiki";
+            break;
+        case "selector-button-internet-archive":
+            selectorButton.style.backgroundImage = "url('imgs/logos/internet\ archive\ logo.png')";
+            selectedBaseUrl = internetArchiveBaseUrl;
+            searchQuery.placeholder = "Search the Internet Archive";
+            break;
+        case "selector-button-wayback-machine":
+            selectorButton.style.backgroundImage = "url('imgs/logos/wayback\ machine\ logo.png')";
+            selectedBaseUrl = waybackMachineBaseUrl;
+            searchQuery.placeholder = "Search the Wayback Machine";
+            break;
+        case "selector-button-wikipedia":
+            selectorButton.style.backgroundImage = "url('imgs/logos/wikipedia\ logo.png')";
+            selectedBaseUrl = wikipediaBaseUrl;
+            searchQuery.placeholder = "Search Wikipedia, The Free Encyclopedia";
+            break;
+        case "selector-button-wikimedia-commons":
+            selectorButton.style.backgroundImage = "url('imgs/logos/wikimedia\ commons\ logo.png')";
+            selectedBaseUrl = wikimediaCommonsBaseUrl;
+            searchQuery.placeholder = "Search Wikimedia Commons";
+            break;
+    }
+}
+
+// Add necessary event listeners
+searchQuery.addEventListener("keypress", performSearch);
+selectorButtonGoogle.addEventListener('click', changeSelectedSearchEngine);
+selectorButtonArchWiki.addEventListener('click', changeSelectedSearchEngine);
+selectorButtonInternetArchive.addEventListener('click', changeSelectedSearchEngine);
+selectorButtonWaybackMachine.addEventListener('click', changeSelectedSearchEngine);
+selectorButtonWikipedia.addEventListener('click', changeSelectedSearchEngine);
+selectorButtoWikimediaCommons.addEventListener('click', changeSelectedSearchEngine);
+
+// Set google as default engine
+selectorButtonGoogle.click();
---
+++ rss feed.js
@@ -0,0 +1,73 @@
+// Strip all HTML, breaks formatting
+function strip(html) {
+    let doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+}
+
+function loadFeed(feed, url, title) {
+    var content = document.getElementById(feed);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var data = JSON.parse(xhr.responseText);
+            var itemsContainer = document.createElement('DIV');
+
+            if (data.status == 'ok') {
+
+
+                for (var i = 0, t = data.items.length; i < t; ++i) {
+                    var item = data.items[i];
+                    var itemContainer = document.createElement('DIV');
+                    itemContainer.classList.add("feed-card");
+
+                    var itemThumbnailElement = document.createElement('IMG')
+                    var itemTitleElement = document.createElement('H2');
+                    var itemLinkElement = document.createElement('A');
+                    var itemDescriptionElement = document.createElement('P');
+
+                    itemTitleElement.classList.add("feed-card-title");
+                    itemLinkElement.classList.add("feed-card-link");
+                    itemDescriptionElement.classList.add("feed-card-desc");
+
+                    itemThumbnailElement.setAttribute('src', item.thumbnail);
+
+                    itemLinkElement.setAttribute('href', item.link);
+                    itemLinkElement.innerText = item.title;
+                    itemTitleElement.appendChild(itemLinkElement);
+
+                    // Removes first image from description so it can be used as thumbnails instead, then makes "Read more" uniform
+                    itemDescriptionElement.innerHTML = item.description.replace(/<img[^>]*>/, "").replace("Read more...", "Read More");
+
+                    itemContainer.appendChild(itemThumbnailElement);
+                    itemContainer.appendChild(itemTitleElement);
+                    itemContainer.appendChild(itemDescriptionElement);
+
+                    itemsContainer.appendChild(itemContainer);
+
+                }
+
+                var titleElement = document.createElement('H1');
+                titleElement.classList.add("feed-title");
+                // titleElement.innerText = data.feed.title;
+                titleElement.innerText = title;
+
+                content.appendChild(titleElement);
+                content.appendChild(itemsContainer);
+            }
+
+            console.log(data);
+        }
+    };
+    xhr.open(
+        'GET',
+        url,
+        true
+    );
+    xhr.send();
+}
+
+loadFeed("rss-feed-left", 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fkotaku.com%2Frss', "Kotaku");
+loadFeed("rss-feed-middle", 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.rockpapershotgun.com%2Ffeed%2Fnews', "Rock Paper Shotgun");
+loadFeed("rss-feed-right", 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.vg247.com%2Ffeed', "VG247");
